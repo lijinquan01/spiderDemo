701616509e504a2a4ac9a95b9a7071c07ed6ec2a
lijinquan <qaz342713873@qq.com>
2018-03-28 16:10:12 +0800

    maven项目雏形

---
+++ src/main/java/com/spider/JsoupSpider.java
@@ -0,0 +1,16 @@
+package com.spider;
+
+/**
+ * <p>
+ * 主函数启动爬虫
+ * <p>
+ *
+ * @author 2018/3/28 0028
+ **/
+public class JsoupSpider
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
---
+++ src/main/java/com/spider/JsoupUtils.java
@@ -0,0 +1,13 @@
+package com.spider;
+
+/**
+ * <p>
+ * 爬虫工具方法集
+ * <p>
+ *
+ * @author 2018/3/28 0028
+ **/
+public class JsoupUtils
+{
+
+}
