13c3743f9936ba848acfaefdf96b7143944fde00
lijinquan <qaz342713873@qq.com>
2018-03-28 21:22:50 +0800

    Merge branch 'master' of github.com:lijinquan01/spiderDemo
    冲突暂时未解决aaaa
    
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

---
+++ src/main/java/com/spider/domain/Book.java
@@ -0,0 +1,45 @@
+package com.spider.domain;
+
+/**
+ * <p>
+ * 一句话描述
+ * <p>
+ *
+ * @author 2018/3/28 0028
+ **/
+public class Book
+{
+	private String bookId;
+	private String bookPrice;
+	private String bookName;
+
+	public String getBookId()
+	{
+		return bookId;
+	}
+
+	public void setBookId(String bookId)
+	{
+		this.bookId = bookId;
+	}
+
+	public String getBookPrice()
+	{
+		return bookPrice;
+	}
+
+	public void setBookPrice(String bookPrice)
+	{
+		this.bookPrice = bookPrice;
+	}
+
+	public String getBookName()
+	{
+		return bookName;
+	}
+
+	public void setBookName(String bookName)
+	{
+		this.bookName = bookName;
+	}
+}
